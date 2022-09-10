a6dd1a45a2fbe9bd83de55a833b1881f09c9a7ff
pc-4nt04n <4nt04n@gmail.com>
2022-09-10 18:31:57 -0300

    Update bill is working  mtf!!!!!!!!!

--- src/main/java/org/mugiwaras/backend/integration/cli2/model/persistence/BillDetailRepository.java
+++ src/main/java/org/mugiwaras/backend/integration/cli2/model/persistence/BillDetailRepository.java
@@ -1,10 +1,18 @@
 package org.mugiwaras.backend.integration.cli2.model.persistence;
 
 import org.mugiwaras.backend.integration.cli2.model.BillDetail;
+import org.mugiwaras.backend.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BillDetailRepository  extends JpaRepository<BillDetail, Long> {
-
+    List<BillDetail> findAllById_IdBill(long idBill);
 }
+
